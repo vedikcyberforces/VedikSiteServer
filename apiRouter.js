2eb56976d6d7f381cdfcef7e523dab63f5da2143
ankit <ac2000choudhary@gmail.com>
2021-05-19 16:01:58 +0530

    Art Gallery Images Uploading to cloud storage

--- apiRouter.js
+++ apiRouter.js
@@ -10,13 +10,13 @@ const PostsAPI = require("./dataRoutes/postsAPI")
 const ArtAPI = require("./dataRoutes/artAPI")
 
 
-// const multipart = require('connect-multiparty');
-// const multipartMiddleware = multipart({
-//     storage: 'vedik'
-// });
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({
+    storage: 'vedik'
+});
 
-// var azure = require('azure-storage');
-// var blobStorage = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=vedikstorage;AccountKey=7OmeGweYiFuBYd+/nzBAj3chFHo0vycwFc95s2yfbYpDOEbVPEnHY1lI5KWZms2G7O1//8F1JmGt2jcueom8jg==;EndpointSuffix=core.windows.net");
+var azure = require('azure-storage');
+var blobStorage = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=vedikstorage;AccountKey=7OmeGweYiFuBYd+/nzBAj3chFHo0vycwFc95s2yfbYpDOEbVPEnHY1lI5KWZms2G7O1//8F1JmGt2jcueom8jg==;EndpointSuffix=core.windows.net");
 
 mongoose.set('useFindAndModify', false);
 
@@ -35,7 +35,7 @@ to news component will be Handled Here
 const newsModel = mongoose.model("News", schemas.newsSchema)
 API.route("/news")
     .get((req, res) => {
-        newsAPI.getNews(newsModel).then((val) => {res.send(val);});
+        newsAPI.getNews(newsModel).then((val) => { res.send(val); });
     })
     .post((req, res) => {
         try {
@@ -61,8 +61,8 @@ Profile Section All POST related
 to members component will be Handled Here
 */
 
-API.get("/member", (req, res)=>{
-    memberApi.getMember(MemberModal).then((val)=>{res.send(val)}).catch((err)=>{console.log(err)})
+API.get("/member", (req, res) => {
+    memberApi.getMember(MemberModal).then((val) => { res.send(val) }).catch((err) => { console.log(err) })
 })
 
 /*
@@ -76,20 +76,19 @@ Profile Section All GET and POST related
 to profile component will be Handled Here
 */
 API.route('/profile')
-        .get( (req, res) => {
-            ProfileAPI.getProfile(MemberModal, req.query.username).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
-        })
-        .post( async (req, res) => {
-            try{
-                if(req.body != null)
-                {
-                    ProfileAPI.updateProfile(MemberModal, req.body, req.query.username).then((val) =>{res.send(val)}).catch((err) =>{console.error(err)})
-                }
-            } catch(err){
-                res.status(400)
-                console.log(err)
+    .get((req, res) => {
+        ProfileAPI.getProfile(MemberModal, req.query.username).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
+    })
+    .post(async (req, res) => {
+        try {
+            if (req.body != null) {
+                ProfileAPI.updateProfile(MemberModal, req.body, req.query.username).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
             }
-        })
+        } catch (err) {
+            res.status(400)
+            console.log(err)
+        }
+    })
 
 /*
 End of Profile Section
@@ -102,54 +101,53 @@ to profile component will be Handled Here
  */
 
 API.route('/projects')
-        .get( (req, res) => {
+    .get((req, res) => {
 
-            if(req.query.id) {
-                //To get the Projects by id
-                ProjectsAPI.getProjectsById(projectsModal, req.query.id).then((val) => {res.send(val)}).catch(err => {console.error(err)})
+        if (req.query.id) {
+            //To get the Projects by id
+            ProjectsAPI.getProjectsById(projectsModal, req.query.id).then((val) => { res.send(val) }).catch(err => { console.error(err) })
+        }
+        else {
+            //To get the Project
+            ProjectsAPI.getProjects(projectsModal).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
+        }
+
+    })
+    .post((req, res) => {
+        try {
+            if (req.body != null) {
+                //To insert the project
+                const project = new projectsModal({
+                    name: req.body.name,
+                    about: req.body.about,
+                    images: req.body.images,
+                    icon: req.body.icon,
+                    developer: req.body.developer,
+                    title: req.body.title,
+                    time: req.body.time
+                })
+
+                ProjectsAPI.insertProjects(project).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
+
+
+                //To Delete the Projects
+                // projectsModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
+                //     if(err)
+                //     {
+                //         res.status(400)
+                //     }
+                //     else{
+                //         res.json(object)
+                //     }
+                // })
+
             }
-            else{
-                //To get the Project
-                ProjectsAPI.getProjects(projectsModal).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
-            }
-            
-        })
-        .post( (req, res) => {
-            try {
-                if(req.body != null)
-                {
-                    //To insert the project
-                    const project = new projectsModal({
-                        name : req.body.name,
-                        about : req.body.about,
-                        images : req.body.images,
-                        icon : req.body.icon,
-                        developer : req.body.developer,
-                        title : req.body.title,
-                        time: req.body.time
-                    })
 
-                    ProjectsAPI.insertProjects(project).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
-
-
-                    //To Delete the Projects
-                    // projectsModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
-                    //     if(err)
-                    //     {
-                    //         res.status(400)
-                    //     }
-                    //     else{
-                    //         res.json(object)
-                    //     }
-                    // })
-
-                }
-
-            } catch (err) {
-                res.status(400)
-                console.error(err)
-            }
-        })
+        } catch (err) {
+            res.status(400)
+            console.error(err)
+        }
+    })
 
 /*
 End of Project Section
@@ -163,43 +161,42 @@ to post component will be Handled Here
  */
 
 API.route('/posts')
-        .get( (req, res) => {
+    .get((req, res) => {
 
-            PostsAPI.getPost(postsModal).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
-            
-        })
-        .post( (req, res) => {
-            try {
-                if(req.body != null)
-                {
-                    // //To add the post
-                    const post = new postsModal({
-                        title : req.body.title,
-                        content : req.body.content,
-                        by : req.body.username,
-                        time : req.body.time
-                    })
+        PostsAPI.getPost(postsModal).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
 
-                    PostsAPI.insertPost(post).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
+    })
+    .post((req, res) => {
+        try {
+            if (req.body != null) {
+                // //To add the post
+                const post = new postsModal({
+                    title: req.body.title,
+                    content: req.body.content,
+                    by: req.body.username,
+                    time: req.body.time
+                })
 
-                    // To Delete the Post
-                    // postsModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
-                    //     if(err)
-                    //     {
-                    //         res.status(400)
-                    //     }
-                    //     else{
-                    //         res.json(object)
-                    //     }
-                    // })
+                PostsAPI.insertPost(post).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
 
-                }
+                // To Delete the Post
+                // postsModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
+                //     if(err)
+                //     {
+                //         res.status(400)
+                //     }
+                //     else{
+                //         res.json(object)
+                //     }
+                // })
 
-            } catch (err) {
-                res.status(400)
-                console.error(err)
             }
-        })
+
+        } catch (err) {
+            res.status(400)
+            console.error(err)
+        }
+    })
 
 /*
 End of Post Section
@@ -213,41 +210,54 @@ to Art Gallery component will be Handled Here
  */
 
 API.route('/art')
-        .get( (req, res) => {
+    .get((req, res) => {
 
-            ArtAPI.getArt(artModal).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
-            
-        })
-        .post( (req, res) => {
-            try {
-                if(req.body != null)
-                {
-                    //To add the art
-                    const art = new artModal({
-                        image : req.body.image,
-                        time : req.body.time
-                    })
+        ArtAPI.getArt(artModal).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
 
-                    ArtAPI.insertArt(art).then((val) => {res.send(val)}).catch((err) => {console.error(err)})
+    })
+    .post(multipartMiddleware, (req, res) => {
+        try {
+            if (req.body != null) {
 
-                    // To Delete the Art
-                    // artModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
-                    //     if(err)
-                    //     {
-                    //         res.status(400)
-                    //     }
-                    //     else{
-                    //         res.json(object)
-                    //     }
-                    // })
+                //storing the art Gallery Image/photos in cloud storage
+                
+                blobStorage.createBlockBlobFromLocalFile('artgallery', req.files.file.name, req.files.file.path, function (err, result, response) {
+                    if (!err) {
 
-                }
+                        var image = blobStorage.getUrl('artgallery', req.files.file.name)
+                        console.log(image)
+                        
+                        //To add the art
+                        const art = new artModal({
+                            image: image,
+                            time: req.body.time
+                        })
 
-            } catch (err) {
-                res.status(400)
-                console.error(err)
+                        ArtAPI.insertArt(art).then((val) => { res.send(val) }).catch((err) => { console.error(err) })
+                    }
+                    else {
+                        console.log(err);
+                    }
+                });
+
+                // To Delete the Art
+                // artModal.findOneAndDelete({_id: req.query.id}, (err, object) => {
+                //     if(err)
+                //     {
+                //         res.status(400)
+                //     }
+                //     else{
+                //         res.json(object)
+                //     }
+                // })
+
             }
-        })
+
+        } catch (err) {
+            res.status(400)
+            console.error(err)
+        }
+    })
 
 /*
 End of Art Section
